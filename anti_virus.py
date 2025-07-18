12f1e32fab9f6b4a22cbeb5cb3cd03415018ae4a
וע <nadav.moshkovits@gmail.com>
2025-07-18 18:01:48 +0300

    traverse files function

--- anti_virus.py
+++ anti_virus.py
@@ -1,1 +1,17 @@
-check=1
+from pathlib import Path
+# d:\Videos\Lockdownprotocol
+
+def traverse_files(file_path):
+    for path in Path(file_path).iterdir:
+        if path.is_dir():
+            flag = traverse_files(path)
+        else:
+            flag = other_func(path)
+        if not flag:
+            return False
+        
+    return True
+
+
+def other_func(file):
+    pass
